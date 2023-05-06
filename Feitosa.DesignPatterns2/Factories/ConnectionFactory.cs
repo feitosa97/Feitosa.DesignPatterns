e3a63195771d85fa4e0e971a7e7c5a2470dbb0c6
Lucas Feitosa <lucasf.s@icloud.com>
2023-05-06 15:45:11 -0300

    creating a db factory

---
+++ Feitosa.DesignPatterns2/Factories/ConnectionFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Feitosa.DesignPatterns2.Factories
+{
+    public class ConnectionFactory
+    {
+        public IDbConnection GetConnection()
+        {
+            var connection = new SqlConnection();
+            connection.ConnectionString = "User Id=sa;Password=Senha123;Server=localhost;Database=master;";
+            connection.Open();
+            return connection;
+        }
+    }
+}
---
+++ Feitosa.DesignPatterns2/Program.cs
@@ -0,0 +1,22 @@
+﻿using Feitosa.DesignPatterns2.Factories;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Data;
+
+namespace Feitosa.DesignPatterns2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IDbConnection connection = new ConnectionFactory().GetConnection();
+
+            IDbCommand command = connection.CreateCommand();
+            command.CommandText = "Select * From Sys.Tables";
+            command.CommandType = CommandType.Text;
+            command.ExecuteNonQuery();
+
+
+        }
+    }
+}
